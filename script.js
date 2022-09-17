9d3106888ae654a6d825c6e91dacc9becceda177
Ar-Akhil <ar.akhil1511@gmail.com>
2022-09-17 20:35:16 +0530

    operation game functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const gameboard = () => {
-  const board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const gameboard = (() => {
+  let board = Array(8);
+  console.log(board);
   const containerBody = document.getElementById("main-container");
   const container = document.createElement("div");
   container.classList.add("game-container");
@@ -10,8 +11,14 @@ const gameboard = () => {
     cells.setAttribute("data-index", i);
     container.appendChild(cells);
   }
-  gameplay();
-};
+  gameplay(board);
+
+  const getField = (index) => {
+    if (index > board.length) return;
+    return board[index];
+  };
+  return { getField };
+})();
 
 function player(symbol) {
   this.symbol = symbol;
@@ -20,23 +27,53 @@ function player(symbol) {
   };
   return { getSymbol };
 }
-function gameplay() {
-  const player1 = player("jhon", "X");
-  const player2 = player("jack", "O");
-  let round = 1;
+function gameplay(board) {
+  const player1 = player("X");
+  const player2 = player("O");
+  let round = 0;
 
   const cells = document.querySelectorAll(".cells");
   cells.forEach((cell) => {
     cell.addEventListener("click", () => {
-      round++;
-      console.log("clicked");
-      getPlayerSymbol();
+      const index = parseInt(cell.dataset.index);
+      if (cell.textContent === "") {
+        round++;
+        cell.textContent = getPlayerSymbol();
+        board.splice(index, 1, cell.textContent);
+        board.join();
+        console.log(index);
+      }
+      const result = document.getElementById("resultText");
+      if (checkWinner(index)) {
+        console.log(index);
+        result.textContent = getPlayerSymbol() + " " + "is the winner";
+        return;
+      } else if (round === 9) {
+        result.textContent = " It's a draw";
+      }
     });
   });
-  console.log(cells);
   const getPlayerSymbol = () => {
-    console.log("turn");
     return round % 2 === 1 ? player1.getSymbol() : player2.getSymbol();
   };
+
+  const checkWinner = (index) => {
+    const winConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    return winConditions
+      .filter((combination) => combination.includes(index))
+      .some((possibleCombination) =>
+        possibleCombination.every(
+          (index) => gameboard.getField(index) === getPlayerSymbol()
+        )
+      );
+  };
 }
-gameboard();
